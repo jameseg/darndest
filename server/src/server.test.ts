797f108085cfef06de130a9a03afc6659e7d03b5
James Gooding <38703245+jameseg@users.noreply.github.com>
2024-12-31 12:54:42 -0500

    update server test add vitest for scripts

--- server/src/server.test.ts
+++ server/src/server.test.ts
@@ -1,14 +1,9 @@
 import request from 'supertest';
-import { describe, expect, it } from 'vitest';
+import { describe, it } from 'vitest';
 import app from './server';
 
 describe('GET /', () => {
-  it('responds with 200', async () => {
-    const response = await fetch('http://localhost:5200/');
-    expect(response.status).toBe(200);
-  });
-
-  it('responds with Hello World', () => {
+  it('responds successfully with Hello World', () => {
     request(app).get('/').expect(200).expect('Hello World');
   });
 });
